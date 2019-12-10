df62f542641e00d56dec0a120a2fd4df67566410
wkimdev <wkimdev@gmail.com>
2019-12-10 19:10:11 +0900

    first data change duration test ing

--- app.js
+++ app.js
@@ -2,65 +2,161 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var axios = require('axios');
-var callcuesheet = require('./callcuesheet.js');
 
 /**
- * 큐시트 시간 수정
- * 
- * param : albumId, starttime, dutaion
- * 
- * data[0].cuesheetItems > itemInfo[5] > duration
- * 
- * api에서 파일을 읽는다
- * 요청받은 param에 대한 albumId검증 이후(dummy라 필요없음), 해당 itemInfo no값을 찾아서
- * 1. 그 itemInfo의 duraion수정
- * 2. 그 itemInfo의 '다음'itemInfo (index++)의 starttiem, durtaion수정 
- * 그 다음... 그래서 끝까지 수정 
- * 3. 파일을 쓴다. 
- * 
- * 4. res.send()를 한다. 
- * 
+ * 큐시트 시간 수정 2
  */
-app.get('/api/change/cuesheet/:startTime&:duration', function(req, res) {
-  //var startTime = req.params.startTime;
-  var startTime = '080000'; // 첫번째 시작시간 고정
-  //var duration = req.params.duration;
-  var duration = '0500'; // 변경 duration
+app.get('/api/change/cuesheet', function(req, res) {
+  var startTime = req.query.startTime;
+  var updateDuration = req.query.duration; // 변경 duration
+
   axios.get('http://localhost:3000/aws/api/ws/cuesheet/v2')
-   .then(response => { 
-      // console.log("test : %o", response.data.item.data[0].cuesheetItems)
+   .then(response => {
       var cuesheetItems = response.data.item.data[0].cuesheetItems;
-      // console.log("==============================================")
-      // console.log("cuesheetItems test : %o", cuesheetItems[0])
-      // console.log("startTime test : %o", startTime)
+      var newCuesheetItems = [];
+      var isChange = false;
       var index = 0;
+      var beforeTime = [];
+      var beforeDuration = [];
       for (let item of cuesheetItems) { // 57
-        console.log("tes item : %o", item.itemInfo[3])
-        var currentStartime = item.itemInfo[3]
-        if(startTime == item.itemInfo[3]){
-          console.log('duration value :'+item.itemInfo[4]) // 분 환산
-          item.itemInfo[4] = duration;  // 현재시간의 duration을 업데이트
-          break;
+        beforeTime[index] = item.itemInfo[3];
+
+        if (startTime == item.itemInfo[3] && isChange === false) {
+          item.itemInfo[4] = updateDuration;  // 첫번재 item의 duration을 업데이트
+          beforeDuration[index] = item.itemInfo[4];
+          console.log("beforeDuration[index] : %o", beforeDuration[index]);
+          isChange = true;
+          console.log("duration update completed !!!")
+        } else if (isChange) {
+
+          // 이전 duration 정보를 가져온다. 
+          console.log("test : %o", beforeDuration[index-1]);
+          
+          var beforeValue = parseInt(beforeDuration[index-1].substr(0,2)); // 0900
+          var beforeStartM = parseInt(beforeTime[index-1].substr(2,2)); // 085900
+
+          console.log("beforeValue : %o", beforeValue);
+          console.log("beforeStartM : %o", beforeStartM);
+
+          // 09 + 59
+          var addResult = parseInt(beforeValue + beforeStartM);
+          console.log("addResult : %o", addResult);
+          if(addResult >= 60){
+            console.log('1111')
+            var hourValue = beforeTime[index-1].substr(0,2); // hh
+            var updateHour = parseInt(hourValue) + 1;
+            var hourValue = updateHour.toString();
+            if(hourValue.length < 2) {
+              var updateHourString = "0" + updateHour.toString();
+              item.itemInfo[3] = item.itemInfo[3].replaceAt(0, updateHourString)
+            } else if (hourValue.length == 2) {
+              item.itemInfo[3] = item.itemInfo[3].replaceAt(0, (hourValue))
+            }
+
+            // mm update
+            var minUpdate = ( addResult - 60 );
+            console.log("minUpdate : %o", minUpdate);
+            var minValue = minUpdate.toString();
+            if(minValue.length < 2){
+              minUpdate = "0" + minUpdate.toString();
+              item.itemInfo[3] = item.itemInfo[3].replaceAt(2, (minUpdate.toString()))
+            } else if (minValue.length == 2) {
+              item.itemInfo[3] = item.itemInfo[3].replaceAt(2, (minValue))
+            }
+
+            console.log("update succeed !!! : %o", item.itemInfo[3]);
+          } else if( addResult < 60 ){
+            console.log('2222')
+            var beforeHour = parseInt(beforeTime[index-1].substr(0,2));
+            var currentHour = parseInt(beforeTime[index].substr(0,2));
+
+            console.log('beforeHour : %o', beforeHour);
+            console.log('currentHour : %o', currentHour);
+
+            if( beforeHour == currentHour ){
+              console.log('3333')
+              // mm update
+              console.log('addResult : %o', addResult);
+              // var minUpdate = ( addResult - 60 );
+              var minUpdate = addResult;
+              var minValue = minUpdate.toString();
+              if(minValue.length < 2){
+                minUpdate = "0" + minUpdate.toString();
+                item.itemInfo[3] = item.itemInfo[3].replaceAt(2, (minUpdate.toString()));
+              } else if (minValue.length == 2) {
+                item.itemInfo[3] = item.itemInfo[3].replaceAt(2, (minValue));
+              }
+
+              // item.itemInfo[3] = item.itemInfo[3].replaceAt(2, (minUpdate.toString()));
+              console.log("update succeed !!! : %o", item.itemInfo[3]);
+
+            } else if ( beforeHour < currentHour ) {
+              console.log('4444')
+              // 시간 update
+              var currentHour = currentHour - 1;
+              var currentValue = currentHour.toString();
+              if(currentValue.length < 2) {
+                var updateHourString = "0" + currentHour.toString();
+                 item.itemInfo[3] = item.itemInfo[3].replaceAt(0, updateHourString)
+              } else if (currentValue.length == 2) {
+                 item.itemInfo[3] = item.itemInfo[3].replaceAt(0, (currentValue))
+              }
+              // mm update
+              // 분을 업데이트 시킬때, 기존 분 + 기존 (업데이트된 duration시간으로 바꾸기)
+              var minUpdate = parseInt(beforeTime[index-1].substr(2,2)) + 
+                              parseInt(beforeDuration[index-1].substr(0,2));
+
+              item.itemInfo[3] = item.itemInfo[3].replaceAt(2, (minUpdate.toString()));
+              console.log("update succeed !!! : %o", item.itemInfo[3]);
+            } else if ( beforeHour > currentHour ) {
+              // 09:00 => 09:10 로 넘어가는 경우 
+              console.log('5555')
+
+              var currentHour = currentHour + 1;
+              var currentValue = currentHour.toString();
+              if(currentValue.length < 2) {
+                var updateHourString = "0" + currentHour.toString();
+                 item.itemInfo[3] = item.itemInfo[3].replaceAt(0, updateHourString)
+              } else if (currentValue.length == 2) {
+                 item.itemInfo[3] = item.itemInfo[3].replaceAt(0, (currentValue))
+              }
+              // mm update
+              // 분을 업데이트 시킬때, 기존 분 + 기존 (업데이트된 duration시간으로 바꾸기)
+              var minUpdate = parseInt(beforeTime[index-1].substr(2,2)) + 
+                              parseInt(beforeDuration[index-1].substr(0,2));
+
+              item.itemInfo[3] = item.itemInfo[3].replaceAt(2, (minUpdate.toString()));
+              console.log("update succeed !!! : %o", item.itemInfo[3]);
+            }
+          }
         }
+
+        beforeTime[index] = item.itemInfo[3];
+        beforeDuration[index] = item.itemInfo[4];
+        index++;
+        newCuesheetItems.push(item)
       }
-  
-    })
+      response.data.item.data[0].cuesheetItems = newCuesheetItems;
+      res.send(response.data)
+   })
    .catch(err => console.log(err))
 })
+
+String.prototype.replaceAt=function(index, character) {
+    return this.substr(0, index) + character + this.substr(index+character.length);
+}
 
 function getCuesheet(){
   return app.get('/aws/api/ws/cuesheet/v2', function (req, res) {
     console.log('call WEBUI API quesheets...! 큐시트 변경 확인 API Call...')
-  
-   // 요청이 들어올때마다 랜덤하게 파일 응답
-  
+    
+    /**
+     * 랜덤설정
     var cuesheetFiles = 4;
     var max = cuesheetFiles
     var randomIndex = Math.floor(Math.random() * (max - 0))
-  
-    console.log('[ RandomIndex : %o ]', randomIndex)
-  
-    //fs.readFile('cuesheet_v' + randomIndex + '.json', 'utf-8', function(err, data) {
+   */
+
     fs.readFile('cuesheet_v0.json', 'utf-8', function(err, data) {
       if(err){
           res.end('500 Internal Server Error : '+err);
